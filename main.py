6ce714195bb2c35a4cc5667a54d1d011f5ffc741
Leo Lundqvist <163914364+leoldqv@users.noreply.github.com>
2024-12-18 08:19:18 +0100

    hi

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import discord
 
 from discord.ext import commands
 from colorama import Fore
+from useful import load_data, save_data
+
+settings = load_data("../settings")
 
 intents = discord.Intents.all()
-bot = commands.Bot(intents=intents, command_prefix='e!')
+bot = commands.Bot(intents=intents, command_prefix=settings.get("prefixes"))
+
+if __name__ == "__main__":
+    bot.run(settings.get("token"))
