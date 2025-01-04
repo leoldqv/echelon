0107d4d09d4c9f72e2afcfe1fa3542c038aeb4a0
Leo Lundqvist <163914364+leoldqv@users.noreply.github.com>
2025-01-04 19:06:32 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import discord
+import asyncio
+import shutil
+import sys
+import os
 
 from discord.ext import commands
 from colorama import Fore
@@ -8,6 +12,17 @@ settings = load_data("../settings")
 
 intents = discord.Intents.all()
 bot = commands.Bot(intents=intents, command_prefix=settings.get("prefixes"))
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+def print_centered(s: str):
+    print(s.center(shutil.get_terminal_size().columns))
+
+@bot.event
+async def on_ready():
+    clear_console()
+    #print_centered()
 
 if __name__ == "__main__":
     bot.run(settings.get("token"))
